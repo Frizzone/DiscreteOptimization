658a63ac13aab79450d652339b654edb2db0a4ec
Frizzone <gustavo.l.frizzone@accenture.com>
2021-01-30 11:16:05 -0300

    Graph coloring: clear code

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from ortools.sat.python import cp_model
+import cp_ortool_graphcoloring
 
 class Node:
     def __init__(self, index, value, color):
@@ -33,7 +33,7 @@ def solve_it(input_data):
 
     nodesColor = [-1]*node_count
 
-    solution = ORToolsSolver(edges, node_count)
+    solution = cp_ortool_graphcoloring.ORToolsSolver(edges, node_count)
 
 
     #if(validadeSolution(edges, solution)): print ("A solução é válida")
@@ -45,45 +45,6 @@ def solve_it(input_data):
 
     return output_data
 
-def ORToolsSolver(edges, node_count):
-
-    model = cp_model.CpModel()
-
-    num_colors = node_count
-
-    nodeColors = []
-    for node in range(node_count):
-        nodeColors.append(model.NewIntVar(0, num_colors, str(node)))
-
-    max_color = model.NewIntVar(0, num_colors, 'max_color')
-
-    #restrição de max_color = max(nodeColors)
-    model.AddMaxEquality(max_color, nodeColors)
-    
-    #adicionar restrições de nós adjacentes
-    # se aresta(n1,n2) então nodeColor(n1) != nodeColor(n2)
-    for e in edges:
-        model.Add(nodeColors[e[0]] != nodeColors[e[1]])
-        #usar AllDifferent para melhorar tempo
-
-    # symmetry breaking
-    # model.Add(nodeColors[0] == 1);
-    # model.Add(nodeColors[1] <= 2);
-    #for i in range(num_colors):
-    #    model.Add(nodeColors[i] <= i+1)
-
-    #função objetivo =  minimizar(max_color)
-    model.Minimize(max_color)
-
-    solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 10.0
-    status = solver.Solve(model)
-
-        
-    solution = []
-    for node in range(node_count): solution.append(solver.Value(nodeColors[node]))
-
-    return solution
 
 
 if __name__ == '__main__':
